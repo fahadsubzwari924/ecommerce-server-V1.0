708a8d49f29573cd82bce0847304ec89390fe577
Fahad Subzwari <fahadsubzwari924@gmail.com>
2019-11-13 01:41:11 +0500

    changes

--- src/api/handlers/product.js
+++ src/api/handlers/product.js
@@ -3,74 +3,69 @@
 
 import { generateResponse, parseBody } from "../../utilities";
 import { saveProduct, findAllProducts, findProductById, editProduct, checkGroupByName, checkGroupByColour, removeProduct } from "../../models/product";
-
+const multer = require('multer');
 
 export async function addProduct(req, res) {
-  try {
-    let body = parseBody(req)
-    if (body) {
-      let group = await saveProduct(body)
-      if (group) {
-        generateResponse(group.success, group.message, group.data, res)
-      }
-    } else {
-      generateResponse(false, "Please provide complete info", null, res)
+    try {
+        let body = parseBody(req)
+        if (body) {
+            let group = await saveProduct(body)
+            if (group) {
+                generateResponse(group.success, group.message, group.data, res)
+            }
+        } else {
+            generateResponse(false, "Please provide complete info", null, res)
+        }
+    } catch (err) {
+        generateResponse(false, 'Error occured, 404 not found!', err, res)
     }
-  }
-  catch (err) {
-    generateResponse(false, 'Error occured, 404 not found!', err, res)
-  }
 }
 
 export async function getAllProducts(req, res) {
-  try {
-    let products = await findAllProducts()
-    if (products) {
-      generateResponse(products.success, products.message, products.data, res)
+    try {
+        let products = await findAllProducts()
+        if (products) {
+            generateResponse(products.success, products.message, products.data, res)
+        }
+    } catch (err) {
+        generateResponse(false, 'Error occured, 404 not found!', err, res)
     }
-  }
-  catch (err) {
-    generateResponse(false, 'Error occured, 404 not found!', err, res)
-  }
 }
 
 export async function getProductById(req, res) {
-  try {
-    let id = req.params.id
-    let product = await findProductById(id)
-    if (product) {
-      generateResponse(product.success, product.message, product.data, res)
+    try {
+        let id = req.params.id
+        let product = await findProductById(id)
+        if (product) {
+            generateResponse(product.success, product.message, product.data, res)
+        }
+    } catch (err) {
+        generateResponse(false, 'Error occured, 404 not found!', err, res)
     }
-  }
-  catch (err) {
-    generateResponse(false, 'Error occured, 404 not found!', err, res)
-  }
 }
 
 export async function updateProduct(req, res) {
-  try {
-    let body = parseBody(req)
-    if (body) {
-      let product = await editProduct(body)
-      if (product) {
-        generateResponse(product.success, product.message, null, res)
-      }
+    try {
+        let body = parseBody(req)
+        if (body) {
+            let product = await editProduct(body)
+            if (product) {
+                generateResponse(product.success, product.message, null, res)
+            }
+        } else {
+            generateResponse(false, "Please provide complete info", null, res)
+        }
+    } catch (err) {
+        generateResponse(false, 'Error occured, 404 not found!', err, res)
     }
-    else {
-      generateResponse(false, "Please provide complete info", null, res)
-    }
-  }
-  catch (err) {
-    generateResponse(false, 'Error occured, 404 not found!', err, res)
-  }
 }
 
 export async function deleteProduct(req, res) {
-  let id = req.params.id
-  if (id) {
-    let product = await removeProduct(id)
-    if (product) {
-      generateResponse(product.success, product.message, product.data, res)
+    let id = req.params.id
+    if (id) {
+        let product = await removeProduct(id)
+        if (product) {
+            generateResponse(product.success, product.message, product.data, res)
+        }
     }
-  }
 }
